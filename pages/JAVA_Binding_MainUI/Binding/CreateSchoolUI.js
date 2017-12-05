b862f93fd313d933d7631985264f98035d0fc2c1
JennieCheng <chengjinmail@163.com>
2017-12-05 22:23:20 +0800

    createSchool

--- pages/JAVA_Binding_MainUI/Binding/CreateSchoolUI.js
+++ pages/JAVA_Binding_MainUI/Binding/CreateSchoolUI.js
@@ -10,9 +10,7 @@ Page({
     schoolname: '',
   },
   schoolName: function (e) {
-    this.setData({
-      schoolname: e.detail.value,
-    })
+    this.schoolname=e.detail.value
   },
   confirm:function(e)
   {
@@ -20,9 +18,21 @@ Page({
       key: 'school',
       data: this.schoolname,
     })
-    wx.redirectTo({
-      url: './TeacherBindingUI',
+    wx.getStorage({
+      key: 'student_or_teacher',
+      success: function(res) {
+        if(res.data==1)
+          wx.redirectTo({
+            url: './TeacherBindingUI',
+          })
+          else
+          wx.redirectTo({
+            url: './StudentBindingUI',
+          })
+      },
     })
+    
+    
   },
   /**
    * 生命周期函数--监听页面加载
@@ -46,53 +56,4 @@ Page({
       },
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
