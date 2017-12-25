3ab81dd9a9a580a93955ad55652493e796e561c7
xingb <764856859@qq.com>
2017-12-25 22:54:05 +0800

    增加解密算法
    增加解密算法

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 //app.js
+var WXBizDataCrypt = require('./utils/RdWXBizDataCrypt.js');
+var AppId ='wx0e1ff5086222b3e9';
+var AppSecret = '5599d7f48b4306bd8a3809538fa5323b';
+var app=getApp();
+
 App({
   data: {
-  _userName:'',
-  _userID:'',
-  _userSchool:'',
-  _userType:1,    //0:学生，1：老师
-  _hasSetName:false,
-  _hasSetID:false,
-  _hasSetSchool:false,
-  _schoolCity:'',
-  _schoolProvince:'',
-  _courseID:1,
-  _classID:1,
-  _seminarID:1,
-  _preUrl:'http://localhost:8090'
+    _userName:'',
+    _userID:'',
+    _userSchool:'',
+    _userType:1,    //0:学生，1：老师
+    _hasSetName:false,
+    _hasSetID:false,
+    _hasSetSchool:false,
+    _schoolCity:'',
+    _schoolProvince:'',
+    _courseID:1,
+    _classID:1,
+    _seminarID:1,
+    _preUrl:'http://localhost:8090'
   },
   onLaunch: function () {
     // 展示本地存储能力
@@ -21,38 +26,51 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    var that=this;
+
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        // if(res.code){
-        //   console.log(res);
-        //   wx.request({
-        //     url: app.data._preUrl+'/auth/weChat',
-        //     data:{
-        //       code:res.code
-        //     },
-        //     header:{
-        //       "content-type":"application/x-www-form-urlencoded"
-        //     },
-        //     method:'POST',
-        //     success:function(res){
-        //       console.log(res.data);
-        //       //var data=res.data;
-        //       //var json=JSON.parse(data)
-        //       //保存3rdSession到storage中
-        //       wx.setStorage({
-        //         key: 'thirdSession',
-        //         data: res.data.thridSession,
-        //       })
-        //     },
-        //     fail:function(res){
-        //       console.log(res);
-        //     }
-        //   })
-        // }else{
-        //   console.log('获取用户登录失败：'+res.errMsg);
-        // }
+        if(res.code){
+          console.log(res);
+          wx.request({
+            url: app.data._preUrl+'/auth/weChat',
+            data:{
+              code:res.code
+            },
+            header:{
+              "content-type":"application/x-www-form-urlencoded"
+            },
+            method:'POST',
+            success:function(res){
+              console.log(res.data);
+              //保存3rdSession到storage中
+              var pc=new WXBizDataCrypt(AppId,res.data.session_key);
+              wx.getUserInfo({
+                success: function (res) {
+                  var dedata = pc.decryptData(res.encryptedData, res.iv);
+                  that.setData({openId:dedata.openId});
+                  //保存解密后的信息到storage中
+                  wx.setStorage({
+                    key: 'decryptData',
+                    data: dedata,
+                  })
+                  console.log('解密后 data: ', data);
+                }
+              })
+              wx.setStorage({
+                key: 'thirdSession',
+                data: res.data.thridSession,
+              });
+            },
+            fail:function(res){
+              console.log(res);
+            }
+          })
+        }else{
+          console.log('获取用户登录失败：'+res.errMsg);
+        }
       }
     })
     // 获取用户信息
