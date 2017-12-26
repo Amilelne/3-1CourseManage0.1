a0d401506665f922918250eabead1ebe752d4b5c
bervely <764856859@qq.com>
2017-12-26 15:33:11 +0800

    登陆
    登陆

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //app.js
 var WXBizDataCrypt = require('./utils/RdWXBizDataCrypt.js');
 var AppId ='wx0e1ff5086222b3e9';
-var AppSecret = '5599d7f48b4306bd8a3809538fa5323b';
+var AppSecret = 'c0bc5f7ab3886cf2124e676b5c0df1b5';
 var that = this;
 
 App({
@@ -19,6 +19,7 @@ App({
     _classID:1,
     _seminarID:1,
     _preUrl:'http://localhost:8080',
+    _code:''
   },
   
   onLaunch: function () {
@@ -27,105 +28,88 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        if(res.code){
-          //var that=this;
-          wx.request({
-            url: 'http://localhost:8080/auth/weChat',
-            data:{
-              code:res.code,
-              "type": 0,
-            },
-            header:{
-              "content-type":"application/json"
-            },
-            method:'POST',
-            success:function(res){
-              console.log(res.data);
-              wx.setStorage({
-                key: 'jwt',
-                data: res.data.jwt,
-              });
-              wx.setStorage({
-                key: 'openid',
-                data: res.data.openid,
-              });
-              wx.setStorage({
-                key: 'userId',
-                data: res.data.userId,
-              });
-              if(res.data.status){
-                wx.navigateTo({
-                  url: '../Binding/ChooseCharacter',
-                })
-              }else{
-                wx.request({
-                  url: 'http://localhost:8080/me',
-                  data: {
-                    jwt: res.data.jwt,
-                  },
-                  header: {
-                    "content-type": "application/json"
-                  },
-                  method: 'GET',
-                  success: function (res) {
-                    console.log(res);
-                  }
-                })
-                wx.navigateTo({
-                  url: '../Binding/ChooseCharacter',
-                })
-              }
-              // app.data._openId = res.data.openid;
-              // app.data._userId = res.data.userId;
-              // var _openId = res.data.openid;
-              // var _userId = res.data.userId;
-              // that.setData(_openId);
-              // that.setData(_userId);
-              // that.setData({
-              //   _openId:"res.data.openid",
-              //   _userId:"res.data.userId",
-              // });
-              // app.data._openId = res.data.openid;
-              // app.data._userId = res.data.userId;
-              // app.setData({
-              //   _openId:res.data.openid,
-              //   _userId:res.data.userId,
-              // });
-            },
-            fail:function(res){
-              console.log(res);
-            }
-          })
-        }else{
-          console.log('获取用户登录失败：'+res.errMsg);
-        }
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              console.log(res);
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+  //   // 登录
+  //   wx.login({
+  //     success: res => {
+  //       // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //       if(res.code){
+  //         console.log(res.code);
+  //         var that=this;
+  //         wx.request({
+  //           url: 'http://localhost:8080/auth/weChat',
+  //           data:{
+  //             code:res.code,
+  //             "type": 0,
+  //           },
+  //           header:{
+  //             "content-type":"application/json"
+  //           },
+  //           method:'POST',
+  //           success:function(res){
+  //             console.log(res.data);
+  //             wx.setStorage({
+  //               key: 'jwt',
+  //               data: res.data.jwt,
+  //             });
+  //             wx.setStorage({
+  //               key: 'openid',
+  //               data: res.data.openid,
+  //             });
+  //             wx.setStorage({
+  //               key: 'userId',
+  //               data: res.data.userId,
+  //             });
+  //             if(res.data.status){
+  //               wx.navigateTo({
+  //                 url: '../Binding/ChooseCharacter',
+  //               })
+  //             }else{
+  //               wx.request({
+  //                 url: 'http://localhost:8080/me',
+  //                 header: {
+  //                   "content-type": "application/json",
+  //                   "Authorization": "Bearer " + res.data.jwt,
+  //                 },
+  //                 method: 'GET',
+  //                 success: function (res) {
+  //                   console.log(res);
+  //                 }
+  //               })
+  //               wx.navigateTo({
+  //                 url: '../Binding/ChooseCharacter',
+  //               })
+  //             }
+  //           },
+  //           fail:function(res){
+  //             console.log(res);
+  //           }
+  //         })
+  //       }else{
+  //         console.log('获取用户登录失败：'+res.errMsg);
+  //       }
+  //     }
+  //   })
+  //   // 获取用户信息
+  //   wx.getSetting({
+  //     success: res => {
+  //       if (res.authSetting['scope.userInfo']) {
+  //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+  //         wx.getUserInfo({
+  //           success: res => {
+  //             console.log(res);
+  //             // 可以将 res 发送给后台解码出 unionId
+  //             this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+  //             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+  //             // 所以此处加入 callback 以防止这种情况
+  //             if (this.userInfoReadyCallback) {
+  //               this.userInfoReadyCallback(res)
+  //             }
+  //           }
+  //         })
+  //       }
+  //     }
+  //   })
   },
   globalData: {
     userInfo: null
--- pages/JAVA_Binding_MainUI/Binding/ChooseCharacter.js
+++ pages/JAVA_Binding_MainUI/Binding/ChooseCharacter.js
@@ -2,30 +2,140 @@
 Page({
 
   data: {},
+
   TeacherButton:function(){
-    var app=getApp()
-    app.data._userType=1,
+    var app=getApp();
+    app.data._userType=1;
+    wx.request({
+      url: 'http://localhost:8080/auth/weChat',
+      data: {
+        code: app.data._code,
+        "type": 1,//app.data._userType
+      },
+      header: {
+        "content-type": "application/json"
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log(res.data);
+        wx.setStorage({
+          key: 'jwt',
+          data: res.data.jwt,
+        });
+        wx.setStorage({
+          key: 'openid',
+          data: res.data.openid,
+        });
+        wx.setStorage({
+          key: 'userId',
+          data: res.data.userId,
+        });
+        if (res.data.status=='unbind') {
+          wx.navigateTo({
+            url: '../Binding/TeacherBindingUI',
+          })
+        } else {
+          wx.navigateTo({
+            url: '../TeacherMainUI/TeacherMainUI',
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    });
     wx.removeStorage({
       key: 'school',
       success: function(res) {},
-    })
-    wx.navigateTo({
-      url:'../Binding/TeacherBindingUI',
-    })
+    });
   },
+
   StudentButton: function () {
-    var app = getApp()
-    app.data._userType = 0,
+    var app = getApp();
+    app.data._userType = 0;
+    wx.request({
+      url: 'http://localhost:8080/auth/weChat',
+      data: {
+        code: app.data._code,
+        "type": 0,//app.data._userType
+      },
+      header: {
+        "content-type": "application/json"
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log(res.data);
+        wx.setStorage({
+          key: 'jwt',
+          data: res.data.jwt,
+        });
+        wx.setStorage({
+          key: 'openid',
+          data: res.data.openid,
+        });
+        wx.setStorage({
+          key: 'userId',
+          data: res.data.userId,
+        });
+        if (res.data.status == 'unbind') {
+          wx.navigateTo({
+            url: '../Binding/StudentBindingUI',
+          })
+        } else {
+          wx.navigateTo({
+            url: '../StudentMainUI/StudentMainUI',
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    });
     wx.removeStorage({
       key: 'school',
       success: function (res) { },
     })
-    wx.navigateTo({
-      url: '../Binding/StudentBindingUI',
+  },
+
+  onLoad: function (options) {
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res);
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          var app=getApp();
+          app.data._code=res.code;
+        } else {
+          console.log('获取用户登录失败：' + res.errMsg);
+        }
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log(res);
+              // 可以将 res 发送给后台解码出 unionId
+              var app=getApp();
+              app.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                var app = getApp();
+                app.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
     })
   },
-  onLoad: function (options) {
-  },
+
   onReady:function(options){
 
   }
