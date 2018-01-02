1c43b7e76c861325895dce4430f22970e0e8f828
echo999999 <764856859@qq.com>
2018-01-02 23:25:09 +0800

    a
    a

--- pages/JAVA_Binding_MainUI/Binding/ChooseCharacter.js
+++ pages/JAVA_Binding_MainUI/Binding/ChooseCharacter.js
@@ -127,34 +127,34 @@ Page({
       }
     });
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              console.log(res);
-              // 可以将 res 发送给后台解码出 unionId
-              var app=getApp();
-              app.globalData.userInfo = res.userInfo
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           console.log(res);
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           var app=getApp();
+    //           app.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                var app = getApp();
-                app.userInfoReadyCallback(res)
-              }
-            },
-            fail: function (res) {
-              console.log("wx.getUserInfo失败", res);
-            }
-          })
-        }
-      },
-      fail:function(res){
-        console.log("wx.getSetting失败",res);
-      }
-    })
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             var app = getApp();
+    //             app.userInfoReadyCallback(res)
+    //           }
+    //         },
+    //         fail: function (res) {
+    //           console.log("wx.getUserInfo失败", res);
+    //         }
+    //       })
+    //     }
+    //   },
+    //   fail:function(res){
+    //     console.log("wx.getSetting失败",res);
+    //   }
+    // })
   },
 
   onReady:function(options){
